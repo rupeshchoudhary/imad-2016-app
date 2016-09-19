44c7b4ef75297199b5b0df46b81e819c5bf25bfc
Rupesh Kumar <rajrupesh405@gmail.com>
2016-09-20 00:22:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,86 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+  title: 'Article One | Rupesh Choudhary ',
+  heading:'Article One',
+  heading1:'Wild life',
+  date:'Sep 5, 2016',
+  content:` 
+          <p>
+          Wildlife traditionally refers to undomesticated animal species, but has come to include all plants, fungi, and other organism
+          that grow or live wild in an area without being introduced by humans.
+          </p>
+          <p> 
+          Wildlife can be found in all ecosystems. Deserts, forests, rain forests, plains, grasslands, and other areas including the most
+          developed urban sites, all have distinct forms of wildlife. While the term in popular culture usually refers to animals that are
+          untouched by human factors, most scientists agree that much wildlife is affected by human activities.
+          </p>`
+};
+
+function createTemplate (data) {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var heading1 = data.heading1;
+    var content = data.content;
+    var htmlTemplate = `
+<html>
+  
+  <head> 
+  <title> ${title} </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link href="/ui/style.css" rel="stylesheet" />
+  
+  </head> 
+  <body>
+      <div class = "container">
+      
+      <div>
+          <a href ="/"> Home</a>
+          
+          
+      </div>
+      <hr/>
+      <h3> 
+      
+      ${heading}
+      
+      </h3>
+      <h3> ${heading1} </h3>
+      <div>
+           ${date}
+          
+      </div>
+      <div> Author = Rupesh Kumar Choudhary </div>
+      
+     
+
+        
+      
+      <div>
+          
+          ${content}
+          
+          
+      </div>
+      </div>
+      
+  </body>
+  </html>
+
+`;
+return htmlTemplate;
+}
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one' , function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
     
 });
 
