8a8b93d5862082c5deab5a1604d92759faf72f75
Rupesh Kumar <rajrupesh405@gmail.com>
2016-09-21 16:57:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,4 @@ var element = document.getElementById('main-text'
   element.innerHTML = 'New value';
   
   //move the image
-  var img = document.getElementById('madi');
-  img.onclick = function () {
-      img.style.marginLeft = '100px';
-  };
+  
