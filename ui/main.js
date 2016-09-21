28602b9b42bceb657a3c93d65283cda9e5cb7561
Rupesh Kumar <rajrupesh405@gmail.com>
2016-09-21 17:16:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 //change the text of the main-text div
 var element = document.getElementById('main-text'
   );
-  element.innerHTML = 'New value';
+  element.innerHTML = 'Rupesh';
   
   //move the image
   var img = document.getElementById('madi');
