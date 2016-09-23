2a41012b060f4f26c7f3e233d68f4187d65151ae
Rupesh Kumar <rajrupesh405@gmail.com>
2016-09-24 02:20:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,22 +1,4 @@
-console.log('Loaded!');
 
-//change the text of the main-text div
-var element = document.getElementById('main-text'
-  );
-  element.innerHTML = 'Rupesh';
-  
-  //move the image
-  var img = document.getElementById('madi');
-  var marginLeft = 0;
-  function moveRight (){
-   marginLeft = marginLeft +1;
-   img.style.marginLeft = marginLeft + 'px';
-  }
-  img.onclick = function () {
-      var interval = setInterval(moveRight, 50);
-      
-  };
-  
   //counter code 
   var button = document.getElementById('counter');
   var counter = 0;
