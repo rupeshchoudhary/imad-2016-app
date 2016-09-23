6efb81ad150d856736f1fba5d922257523bdb932
Rupesh Kumar <rajrupesh405@gmail.com>
2016-09-24 03:02:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,3 +26,18 @@
      
       
   };
+  
+  //submit name
+  var nameInput = document.getElementById('name');
+  var name = nameInput.value;
+  var submit = document.getElementById('submit-btn');
+  submit.onclick = function() {
+      
+     var names = ['name1', 'name2', 'name3', 'name4'];
+     var list = '';
+     for (var i=0; i< names.length; i++){
+         list += '<li>' + names[i] + '</li>';
+     }
+     var ul = document.getElementById('namelist');
+     ul.innerHTML = list;
+  };
