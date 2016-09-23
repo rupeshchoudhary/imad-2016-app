d84387d342d6bd473a5d9bd8326b3f97d14547e5
Rupesh Kumar <rajrupesh405@gmail.com>
2016-09-24 03:19:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -114,6 +114,16 @@ app.get('/counter', function (req,res){
     res.send(counter.toString());
 });
 
+var names = [];
+app.get('/submit-name', function(req, res){
+    //get the name from request
+    var name = req.query.name;
+    
+    names.push(name);
+    // json
+    res.send(JSON.stringify(names));
+});
+
 app.get('/:articleName' , function(req,res){
     //articleName == article-one
     //articles[articleName] == {} content object for article one
@@ -132,16 +142,6 @@ app.get('/ui/main.js', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-var names = [];
-app.get('/submit-name', function(req, res){
-    //get the name from request
-    var name = req.query.name;
-    
-    names.push(name);
-    // json
-    res.send(JSON.stringify(names));
 });
 
 
